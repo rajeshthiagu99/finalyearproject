9fe783d80a4d026d8ec7ccb5aa1ffd601430d086
Rajesh Kumar <rajeshthiagu99@gmail.com>
2021-05-09 09:59:11 +0530

    encode

--- app.py
+++ app.py
@@ -22,6 +22,23 @@ def zone(hr):
     if hr>(0.92*m):
         return 4
 
+def vo2review(vo2max):
+    #vo2max review
+    if vo2max>60:
+        return 'excellent , keep it up'
+
+    if vo2max>52 and vo2max<60:
+        return 'good , slight improvement is needed'
+    
+    if vo2max>47 and vo2max<52:
+        return 'above average ,consistent training will improve your results'
+
+    if vo2max>42 and vo2max<47:
+        return 'average need to work hard'
+
+    if vo2max<42:
+        return 'below average need to take care of your health'
+
 
 app = flask.Flask(__name__)
 
@@ -29,34 +46,34 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['POST'])
 def home():
     output=[]
+
     #getting input
     data = request.json
+
     #converting to dataframes
     df_distances=pd.DataFrame(data['distances'])
     df_heartbeats=pd.DataFrame(data['heartbeats'])
+
     #calculation of vo2 max
     vo2max=15*(max(df_heartbeats['heartbeat'])/min(df_heartbeats['heartbeat']))
-    # results={
-    #     'vo2max':vo2max,
-    #     'max_heartrate':max(df_heartbeats['heartbeat'])
-    # }
     output.append({'name':'vo2max','image':False,'data':vo2max})
+
+    #analysing of vo2 max
+    vo2_review_data=vo2review(vo2max)
+    output.append({'name':'vo2max review','image':False,'data':vo2_review_data})
+
+    #max heartrate
     output.append({'name':'max_heartrate','image':False,'data':max(df_heartbeats['heartbeat'])})
 
     #zone calculation
     df_heartbeats['zones']=df_heartbeats.heartbeat.apply(zone)
-    # results['zones']=json.dumps(list(df_heartbeats['zones']))
-
-
 
     # plotting zone bar graph
     df_heartbeats.zones.plot(kind='bar')
     plt.savefig('zone_bar_chart.png')
     encoded = base64.b64encode(open("zone_bar_chart.png", "rb").read())
     os.remove("zone_bar_chart.png")
-    # results['zone_bar_chart']=encoded.decode("utf-8")
     output.append({'name':'zone_bar_chart','image':True,'data':encoded.decode("utf-8")})
-
 
     #plotting heatmap
     x=df_heartbeats.zones
@@ -64,11 +81,9 @@ def home():
     plt.savefig('heatmap.png')
     encoded = base64.b64encode(open("heatmap.png", "rb").read())
     os.remove("heatmap.png")
-    # results['heatmap']= encoded.decode("utf-8")
     output.append({'name':'heatmap','image':True,'data':encoded.decode("utf-8")})
 
-    
-    
+
     return json.dumps(output)
 
 # if __name__ == "__main__":
